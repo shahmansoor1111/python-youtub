07a13883c92c01c0b78201899743c03706f92d24
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-09-21 17:37:31 +0500

    ok

---
+++ lecture1.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+#//// tupple arr ()
+
+
+
+# n = np.array((1,2,3,4))
+# print(n)
+
+
+#/// list arr []
+
+# n = np.array([1,3,4,])
+# print(n)
+
+#///// 0 dimencail nupmy arr
+
+# n = np.array(50)
+# print(n.ndim)
+
+#/////// 1  dimencail nupmy arr
+
+# n = np.array([50,3])
+# print(n.ndim)
+
+# ///// 2  dimencail nupmy arr
+# n = np.array([[1,2,3], [4,5,6]])
+# print(n)
+# print(n.ndim)
+
+
+# ////numpy array with zero
+
+# /////how many and rows we have
+
+
+# n = np.zeros([2,2])
+# print(n)
+
+# n = np.zeros([4,4])
+# print(n)
+
+# /////data type 
+
+
+# n = np.array([1,2,3,4,5])
+# print(n.dtype)
+
+# n = np.array(["shahmansoor", "khan","ali"],dtype='S5')
+# print(n.dtype)
+
+# ////////conver one data type to another in numpy
+
+# n = np.array([1,2,3,4,5])
+# print(n)
+# print(n.dtype)
+
+
+# n2= n.astype('i')
+# print(n2.dtype)
+
+# ////////aray indexing 
+
+# n = np.array([1,2,3,4,5])
+# print(n[1])
+
+# ///////// aray indexing two dimensial array
+
+# n = np.array([[1,2,3,4],[5,6,7,8]])
+# print(n)
+# print(n[0,0])
+# print(n[0,1])
+# print(n[0,2])
+# print(n[0,3])
+
+
+
+# print(n[1,0])
+# print(n[1,1])
+# print(n[1,2])
+# print(n[1,3])
+
+# n = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+# print(n)
+
+# print(n[0,0,0])
+
+# print(n[1,0,1])
+
+# array slicing 
+
+# n = np.array([1,2,3,4,5,6,7,8,9])
+# print(n[0:7])
+
+
+
+# n = np.array([1,2,3,4,5,6,7,8,9])
+# print(n[3:6])
+
+
+# n = np.array([1,2,3,4,5,6,7,8,9])
+# print(n[:7])
+
+
+# n = np.array([1,2,3,4,5,6,7,8,9])
+# print(n[2:])
+
+
+# n = np.array([1,2,3,4,5,6,7,8,9])
+# print(n[2:5])
+
+# ////////1d aray with step
+n = np.array([1,2,3,4,5,6,7,8,9,10])
+print(n[0:8:2])
+
+
+
+
+
+
+
+
+
+
+
